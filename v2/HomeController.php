d04546f161d3c3976094381bf980c71beedc4ef9
Raffol <eremenko97@bk.ru>
2024-09-28 15:31:29 +0800

    V2Laravel
    2я версия, решил делать на Ларавел

---
+++ v2/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController
+{
+// app/Http/Controllers/HomeController.php
+    public function index() {
+        return view('home');
+    }
+
+}
